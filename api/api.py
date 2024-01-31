bbf6a62bfc48566e0d409264a473f0788b5f4a1e
Likogeles <57628609+Likogeles@users.noreply.github.com>
2024-01-31 10:49:11 +0400

    ready

--- api/api.py
+++ api/api.py
@@ -1,22 +1,50 @@
 from flask import Flask, request
 from random import Random
-
+import psycopg2
 
 app = Flask(__name__)
 
 random_num = None
+
+def create_db():
+    try:
+        conn = psycopg2.connect("dbname=eksdb user=admin password=admin port=5432 host=db")
+        cur = conn.cursor()
+        
+        cur.execute("CREATE TABLE random_nums (num varchar(255));")
+        
+        cur.close()
+        conn.commit()
+        conn.close()
+    except:
+        pass
+
+create_db()
+
+def save_num(new_num):
+    conn = psycopg2.connect("dbname=eksdb user=admin password=admin port=5432 host=db")
+    cur = conn.cursor()
     
+    cur.execute(f"INSERT INTO random_nums VALUES ({new_num});")
+    
+    cur.close()
+    conn.commit()
+    conn.close()
+
+
 @app.route('/')
 def index():
     global random_num
     random = Random()
     random_num = random.randint(10,99)
+    save_num(random_num)
     return {'random_num': random_num}
 
 @app.route('/set_random')
 def set_random():
     global random_num
     random_num = request.args.get('new_num')
+    save_num(random_num)
     return {'random_num': random_num}
 
 @app.route('/hello')
